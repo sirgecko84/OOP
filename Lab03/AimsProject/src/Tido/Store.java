07e7ecaeecf30f72180cd231b22fa0b206715eb6
sirgecko84 <khanhtruong2404@gmail.com>
2024-11-25 21:20:25 +0700

    Add class store

---
+++ Lab03/AimsProject/src/Tido/Store.java
@@ -0,0 +1,57 @@
+package Tido;
+
+public class Store {
+    // Mảng lưu trữ các DVD trong cửa hàng
+    private DigitalVideoDisc[] itemsInStore;
+    private int numberOfDVDs;
+
+    // Constructor khởi tạo Store với số lượng DVD ban đầu
+    public Store(int capacity) {
+        itemsInStore = new DigitalVideoDisc[capacity];
+        numberOfDVDs = 0;
+    }
+
+    // Phương thức thêm DVD vào cửa hàng
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (numberOfDVDs < itemsInStore.length) {
+            itemsInStore[numberOfDVDs] = dvd;
+            numberOfDVDs++;
+            System.out.println(dvd.getTitle() + " has been added to the store.");
+        } else {
+            System.out.println("Store is full, cannot add " + dvd.getTitle());
+        }
+    }
+
+    // Phương thức xóa DVD khỏi cửa hàng
+    public void removeDVD(DigitalVideoDisc dvd) {
+        boolean found = false;
+        for (int i = 0; i < numberOfDVDs; i++) {
+            if (itemsInStore[i].getTitle().equals(dvd.getTitle())) {
+                // Xoá DVD bằng cách dịch chuyển các DVD còn lại
+                for (int j = i; j < numberOfDVDs - 1; j++) {
+                    itemsInStore[j] = itemsInStore[j + 1];
+                }
+                itemsInStore[numberOfDVDs - 1] = null;
+                numberOfDVDs--;
+                System.out.println(dvd.getTitle() + " has been removed from the store.");
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println(dvd.getTitle() + " not found in the store.");
+        }
+    }
+
+    // Phương thức in danh sách các DVD trong cửa hàng
+    public void displayItemsInStore() {
+        if (numberOfDVDs == 0) {
+            System.out.println("The store is empty.");
+        } else {
+            System.out.println("Items in the store:");
+            for (int i = 0; i < numberOfDVDs; i++) {
+                System.out.println(itemsInStore[i].getTitle());
+            }
+        }
+    }
+}
---
+++ Lab03/AimsProject/src/Tido/StoreTest.java
@@ -0,0 +1,27 @@
+package Tido;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        // Tạo cửa hàng với sức chứa 10 DVD
+        Store store = new Store(10);
+
+        // Tạo DVD và thêm vào cửa hàng
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Alters", 87, 19.95f);
+        store.addDVD(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        store.addDVD(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        store.addDVD(dvd3);
+
+        // In danh sách DVD trong cửa hàng
+        store.displayItemsInStore();
+
+        // Xóa DVD khỏi cửa hàng
+        store.removeDVD(dvd2);
+
+        // Kiểm tra lại danh sách DVD trong cửa hàng
+        store.displayItemsInStore();
+    }
+}
