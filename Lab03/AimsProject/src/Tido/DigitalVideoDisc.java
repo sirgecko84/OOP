a123c1c4019a40a0903a5b302c67b22d307b7c41
sirgecko84 <khanhtruong2404@gmail.com>
2024-11-25 15:09:30 +0700

    add class member

--- Lab03/AimsProject/src/Tido/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/Tido/DigitalVideoDisc.java
@@ -1,51 +1,84 @@
 package Tido;
+
 public class DigitalVideoDisc {
-	private String title;
-	private String category;
-	private String director;
-	private int length;
-	private float cost;
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public String getDirector() {
-		return director;
-	}
-	public int getLength() {
-		return length;
-	}
-	public float getCost() {
-		return cost;
-	}
-	public DigitalVideoDisc(String title) {
-		this.title = title;
-	}
-	public DigitalVideoDisc(String category, String title, float cost) {
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-	}
-	public DigitalVideoDisc(String director, String category, String title, float cost) {
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-		this.director = director;
-	}
-	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-		this.director = director;
-		this.length = length;
-	}
-	
-	 //3.	Passing parameter 
-	  //Setter for title
-	 public void setTitle(String title) {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    //5. Classifier member and Instance member
+    private static int nbDigitalVideoDiscs = 0; // class variable to count DVDs
+    private int id; // instance variable to hold the unique id
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // Constructor with title
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;  // assign unique id
+    }
+
+    // Constructor with category, title, and cost
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;  // assign unique id
+    }
+
+    // Constructor with director, category, title, and cost
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;  // assign unique id
+    }
+
+    // Constructor with all attributes
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.director = director;
+        this.length = length;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;  // assign unique id
+    }
+
+    // Setter for title
+    public void setTitle(String title) {
         this.title = title;
     }
 
+    // Getter for nbDigitalVideoDiscs
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
 }
---
+++ Lab03/AimsProject/src/Tido/TestClassMember.java
@@ -0,0 +1,17 @@
+package Tido;
+
+public class TestClassMember {
+    public static void main(String[] args) {
+        // Create some DVDs to test
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Jungle");
+        System.out.println("DVD 1 - ID: " + dvd1.getId() + ", Title: " + dvd1.getTitle());
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Adventure", "The Jungle Adventure", 15.5f);
+        System.out.println("DVD 2 - ID: " + dvd2.getId() + ", Title: " + dvd2.getTitle());
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("John Doe", "Action", "The Big Adventure", 120, 20.0f);
+        System.out.println("DVD 3 - ID: " + dvd3.getId() + ", Title: " + dvd3.getTitle());
+
+        System.out.println("Total number of DigitalVideoDiscs: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
+    }
+}
